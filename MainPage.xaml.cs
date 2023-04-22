95977170a5281831a0f351f3b9196982f97e9a54
Richardgaoxing1 <131502761+Richardgaoxing1@users.noreply.github.com>
2023-04-22 09:56:38 +0800

    MAUI\HelloWord
    MAUI\HelloWord

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWord;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		
+    }
+}
+
---
+++ ServiceLocator.cs
@@ -0,0 +1,18 @@
+﻿using HelloWord.Services;
+using HelloWord.ViewModels;
+
+namespace HelloWord
+{
+    public class ServiceLocator
+    {
+        private IServiceProvider _serviceProvider; 
+        public MainPageViewModel MainPageViewModel=>_serviceProvider.GetService<MainPageViewModel>();//依赖注入 IoC
+        public ServiceLocator() {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<MainPageViewModel>();
+            serviceCollection.AddSingleton<IKeyValueSrorage,KeyValueStorage>();
+            _serviceProvider= serviceCollection.BuildServiceProvider();
+        }
+
+    }
+}
